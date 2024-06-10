d61ea91e0ea0bcf736b372be3c5fa3299ed0bce8
aadarsh-010 <123736056+aadarsh-010@users.noreply.github.com>
2024-06-10 13:13:18 +0530

    Update spline.jsx
    Update spline.jsx

--- Frontend/src/components/spline.jsx
+++ Frontend/src/components/spline.jsx
@@ -1,12 +1,11 @@
+
 import React from 'react';
+
 import Spline from '@splinetool/react-spline';
 
-export default function Splinemodel() {
+export default function App() {
   return (
-    <main style={{width:1000 , height:200, marginTop:0}}>
-      <Spline
-        scene="https://prod.spline.design/vRljTMV34XAZpo8x/scene.splinecode" 
-      />
-    </main>
+    <Spline scene="https://prod.spline.design/vRljTMV34XAZpo8x/scene.splinecode" />
   );
 }
+
