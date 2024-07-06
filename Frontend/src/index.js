89c316f78bf7b69c99f14b227ec0c23d0432d83c
aadarsh-010 <bvmadarsh@gmail.com>
2024-07-06 12:01:55 +0530

    images not loading made major changes vite-> react app

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import './index.css';
+import { Provider } from "react-redux";
+import { persistor, store } from "./store/store.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Proutes from "./components/routes";
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+    <Proutes/>
+    </Provider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
